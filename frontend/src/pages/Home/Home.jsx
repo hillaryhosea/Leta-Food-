44e02b19cc7e74f73d8ffc20b41c155db7f66c3e
hillaryhosea <107868525+hillaryhosea@users.noreply.github.com>
2024-07-03 23:53:50 +0300

    mount header component

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import './Home.css'
 import Header from '../../Components/Header/Header'
+import ExploreMenu from '../../Components/ExploreMenu/ExploreMenu'
+import { useState } from 'react'
 
 const Home = () => {
+
+const [category, setCategory] = useState('All')
+
   return (
     <div> 
     <Header />
+    <ExploreMenu category={category} setCategory ={setCategory}/>
     </div>
   )
 }
