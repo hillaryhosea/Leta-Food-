df660ddbd5da6f459219a977453f439a52c31ef6
hillaryhosea <107868525+hillaryhosea@users.noreply.github.com>
2024-07-05 22:24:59 +0300

    mount food display

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -3,15 +3,17 @@ import './Home.css'
 import Header from '../../Components/Header/Header'
 import ExploreMenu from '../../Components/ExploreMenu/ExploreMenu'
 import { useState } from 'react'
+import { FoodDisplay } from '../../Components/FoodDisplay/FoodDisplay'
 
 const Home = () => {
 
-const [category, setCategory] = useState('All');
+const [category,setCategory] = useState('All');
 
   return (
     <div> 
     <Header />
     <ExploreMenu category={category} setCategory={setCategory}/>
+    <FoodDisplay category={category}/>
     </div>
   )
 }
