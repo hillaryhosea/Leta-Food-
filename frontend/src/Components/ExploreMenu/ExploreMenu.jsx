d916a49fdb665a277ef92e297a750042097c3220
hillaryhosea <107868525+hillaryhosea@users.noreply.github.com>
2024-07-05 22:20:14 +0300

    fuctinality for the active class

--- frontend/src/Components/ExploreMenu/ExploreMenu.jsx
+++ frontend/src/Components/ExploreMenu/ExploreMenu.jsx
@@ -10,7 +10,7 @@ const ExploreMenu = ({category,setCategory}) => {
         <div className='explore-menu-list'>
             {menu_list.map((item, index)=>{
                 return(
-                    <div onClick={()=>setCategory(prev=>prev===item.menu_image?'All':item.menu_name)} key={index} className='explore-menu-list-item' >
+                    <div onClick={()=>setCategory(prev=>prev===item.menu_name?'All':item.menu_name)} key={index} className='explore-menu-list-item' >
                         <img className={category===item.menu_name?'active':''} src={item.menu_image} alt='' />
                         <p> {item.menu_name}</p>
                     </div>
