bd3ba22b5d7e6e2d479e6ac2fe49de7afc1f7757
hillaryhosea <107868525+hillaryhosea@users.noreply.github.com>
2024-07-18 23:46:54 +0300

    menu functionality

--- frontend/src/Components/FoodDisplay/FoodDisplay.jsx
+++ frontend/src/Components/FoodDisplay/FoodDisplay.jsx
@@ -12,7 +12,10 @@ export const FoodDisplay = ({category}) => {
         <h2>Top Dishes near you </h2>
         <div className='food-display-list'>
             {food_list.map((item,index)=>{
+              {console.log(category,item.category);}
+              if(category==="All" || category === item.category){
                 return <FoodItem key={index} id={item.id} name={item.name} description={item.description} price={item.price} image={item.image}/>
+              }
             })}
         </div>
     </div>
