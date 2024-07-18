8aa1fff9bb9373b7921bf7bdc0a9047cab83412e
hillaryhosea <107868525+hillaryhosea@users.noreply.github.com>
2024-07-18 23:48:04 +0300

    cart adjustments

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -18,14 +18,14 @@ const StoreContextProvider = (props) => {
         }
     }
 
-    const removeFromCart =(itemId) =>
+    const removeFromCart = (itemId) =>
         {
             setCartItems((prev)=>({...prev,[itemId]:prev[itemId]-1}))
         }
 
         useEffect (()=>{
             console.log(cartItems);
-        }, [cartItems])
+        },[cartItems])
 
 
     const contextValue = {
