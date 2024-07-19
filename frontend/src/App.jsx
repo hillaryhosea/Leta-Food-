6fde784a73cb9220a0153e939f35d9217b83f0a8
hillaryhosea <107868525+hillaryhosea@users.noreply.github.com>
2024-07-19 22:12:43 +0300

    additonal fragments

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,25 @@
-import React from 'react'
-import Navbar from './Components/Navbar/Navbar'
-import { Route, Routes } from 'react-router-dom'
-import Home from './pages/Home/Home'
-import Cart from './pages/Cart/Cart'
-import PlaceOder from './pages/PlaceOder/PlaceOder'
+import React from "react";
+import Navbar from "./Components/Navbar/Navbar";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Cart from "./pages/Cart/Cart";
+import PlaceOder from "./pages/PlaceOder/PlaceOder";
+import { Footer } from "./Components/Footer/Footer";
 
 const App = () => {
   return (
-    <div className='app'>
-      <Navbar />
-      <Routes>
-        <Route path='/' element={<Home/>} /> 
-        <Route path='/cart' element={<Cart/>} />
-        <Route path='/order' element={<PlaceOder/>} />
-      </Routes>
-    </div>
-  )
-}
+    <>
+      <div className="app">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/order" element={<PlaceOder />} />
+        </Routes>
+      </div>
+      <Footer />
+    </>
+  );
+};
 
-export default App
+export default App;
