921d4bbf0279844d773fa17e37429c1c836f45c0
hillaryhosea <107868525+hillaryhosea@users.noreply.github.com>
2024-07-24 23:22:32 +0300

    adding logins

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "./Components/Navbar/Navbar";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Cart from "./pages/Cart/Cart";
 import PlaceOder from "./pages/PlaceOder/PlaceOder";
 import { Footer } from "./Components/Footer/Footer";
+import { Login } from "./Components/Login/Login";
 
 const App = () => {
+
+  const [showLogin, setShowLogins] = useState(false)
+
   return (
     <>
+    {showLogin?<Login  setShowLogin={setShowLogins}/>:<></>}
       <div className="app">
-        <Navbar />
+        <Navbar setShowLogins={setShowLogins} />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/cart" element={<Cart />} />
